0307c90ea3bd65567fc2b9fc0dcc8fccd70565cc
aluxjeon <aluxj702@gmail.com>
2023-04-10 16:06:25 -0700

    CPSC 210 Project — Phase 4 Task 3

--- src/main/model/Currency.java
+++ src/main/model/Currency.java
@@ -2,12 +2,10 @@ package model;
 
 // Represents a currency with initially 1000 units
 public class Currency {
-    private int currency;
+    private int currency = 1000;
 
     //EFFECTS: Makes a new currency with the initial amount you have to 1000
-    public Currency() {
-        currency = 1000;
-    }
+    public Currency() {}
 
     //EFFECTS: Returns the amount of currency you have
     public Integer getCurrency() {
@@ -19,7 +17,6 @@ public class Currency {
     //EFFECTS: Adds 'amount' to currency and prints out completed to indicate completion
     public void addCurrency(int amount) {
         currency += amount;
-        System.out.println("Completed!");
     }
 
     //REQUIRES: amount >= 0
@@ -27,6 +24,5 @@ public class Currency {
     //EFFECTS: Subtracts 'amount' from currency and prints out completed to indicate completion
     public void subCurrency(int amount) {
         currency -= amount;
-        System.out.println("Completed!");
     }
 }
